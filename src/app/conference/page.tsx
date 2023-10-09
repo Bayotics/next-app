7fbb6240ee47b14728613cf8831e1b5d0f1045a0
bayoticstudios <abdullahishobaloju@gmail.com>
2023-10-09 22:16:36 +0100

    setting up pages for routing

---
+++ src/app/conference/page.tsx
@@ -0,0 +1,8 @@
+
+const Page = () => {
+  return(
+    <div><h1>Welcome to Conference</h1></div>
+  )
+}
+
+export default Page
---
+++ src/app/conference/speakers/page.tsx
@@ -0,0 +1,8 @@
+
+const Page = () => {
+  return(
+    <div><h1>Welcome to conference speakers</h1></div>
+  )
+}
+
+export default Page
